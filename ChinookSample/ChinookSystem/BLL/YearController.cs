2ffd948cb5d3ef3d772145851da042db6323415d
Jessica Brisson <jbrisson2@studentmail.nait.ca>
2016-10-03 17:02:43 -0600

    start to build linq year controller

---
+++ ChinookSample/ChinookSystem/BLL/YearController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel; //ODS
+using ChinookSystem.Data.Entities;
+using ChinookSystem.Data.POCOs;
+using ChinookSystem.DAL; //get to context class
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class YearController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<EmployeeNameList>
+    }
+}
