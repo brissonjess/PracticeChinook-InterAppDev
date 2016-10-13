422fd0297278d6434053db8f54589808a674f072
Jessica Brisson <jbrisson2@studentmail.nait.ca>
2016-10-13 17:00:27 -0600

    setup of startup roles for the application
    SecurityRoles and load method inside the RoleManager

--- ChinookSample/ChinookSystem/Security/RoleManager.cs
+++ ChinookSample/ChinookSystem/Security/RoleManager.cs
@@ -20,5 +20,26 @@ namespace ChinookSystem.Security
         {
                 
         }
+        //this method will be executed with the application starts up under Internet Information Services (IIS)
+        /*
+         *  During the Startup Process
+         *  The 1st time the website is started, a file called Global.aspx is executed
+         *  In this file we will call the method RoleManager to AddStartupRoles()
+         *  
+         *  If you needed to create a new role then you can add it in the SecurityRoles class...
+         */
+        public void AddStartupRoles()
+        {
+            foreach(string roleName in SecurityRoles.StartupSecurityRoles)
+            {
+                //for each item in the StartupSecurityRoles List 
+                //  check if the role already exists in the Secuirty Tables located in the database
+                if(!Roles.Any(r => r.Name.Equals(roleName)))
+                {
+                    //role is not currently on the database
+                    this.Create(new IdentityRole(roleName));
+                }
+            }
+        }
     }
 }
